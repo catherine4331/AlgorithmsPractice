8eb960cbd054dd3a43b0b809f70fe367c18153af
catherine4331 <catherine4331@gmail.com>
2024-09-16 17:29:22 +0100

    Add hashtable approach for twosuM

--- rust/src/twosum.rs
+++ rust/src/twosum.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn brute_force(nums: Vec<i32>, target: i32) -> Vec<i32> {
     for (i, left) in nums.iter().enumerate() {
         for (j, right) in nums.iter().enumerate() {
@@ -15,6 +17,27 @@ pub fn brute_force(nums: Vec<i32>, target: i32) -> Vec<i32> {
     unreachable!("The problem states there should always be a solution")
 }
 
+pub fn hashtable(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut previous = HashMap::new();
+    for (j, num) in nums.iter().enumerate() {
+        if let Some(i) = previous.get(&(target - num)) {
+            return vec![
+                *i,
+                j.try_into()
+                    .expect("The problem won't use arrays longer than i32 max"),
+            ];
+        } else {
+            previous.insert(
+                nums[j],
+                j.try_into()
+                    .expect("The problem won't use arrays longer than i32 max"),
+            );
+        }
+    }
+
+    unreachable!("The problem states there should always be a solution")
+}
+
 #[cfg(test)]
 mod tests {
     use rstest::rstest;
@@ -25,7 +48,8 @@ mod tests {
     #[case(vec![2,7,11,15], 9, vec![0, 1])]
     #[case(vec![3,2,4], 6, vec![1, 2])]
     #[case(vec![3, 3], 6, vec![0, 1])]
-    fn brute_force_test(#[case] input: Vec<i32>, #[case] target: i32, #[case] expected: Vec<i32>) {
-        assert_eq!(brute_force(input, target), expected)
+    fn two_sum_test(#[case] input: Vec<i32>, #[case] target: i32, #[case] expected: Vec<i32>) {
+        assert_eq!(brute_force(input.clone(), target), expected);
+        assert_eq!(hashtable(input, target), expected);
     }
 }
